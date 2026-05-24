526fcf4adb52cd96857d516418f715e6d7186722
Swayam Jethi <swayam.jethi@gmail.com>
2026-05-24 14:09:27 +0530

    reorganize production environment setup in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -8,19 +10,7 @@ import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (process.env.NODE_ENV === "production") {
-  const clientPath = path.join(__dirname, "../client/dist");
-  app.use(express.static(clientPath));
-  app.get("*", (_req, res) => {
-    res.sendFile(path.join(clientPath, "index.html"));
-  });
-}
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +31,16 @@ app.get("/", (_req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const clientPath = path.join(__dirname, "../client/dist");
+  app.use(express.static(clientPath));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
